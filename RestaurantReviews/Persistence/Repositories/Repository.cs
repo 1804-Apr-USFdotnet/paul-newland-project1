34bac2d79a4ab2037b10d439de063b648226dc12
newlandp <paulnewland@hotmail.com>
2018-05-03 11:31:39 -0400

    implemented Repository class

--- RestaurantReviews/Persistence/Repositories/Repository.cs
+++ RestaurantReviews/Persistence/Repositories/Repository.cs
@@ -18,37 +18,37 @@ namespace RestaurantReviews.Persistence.Repositories
 
         public TEntity Get(int id)
         {
-            throw new NotImplementedException();
+            return Context.Set<TEntity>().Find(id);
         }
 
         public IEnumerable<TEntity> GetAll()
         {
-            throw new NotImplementedException();
+            return Context.Set<TEntity>().ToList();
         }
 
         public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return Context.Set<TEntity>().Where(predicate);
         }
 
         public void Add(TEntity entity)
         {
-            throw new NotImplementedException();
+            Context.Set<TEntity>().Add(entity);
         }
 
         public void AddRange(IEnumerable<TEntity> entities)
         {
-            throw new NotImplementedException();
+            Context.Set<TEntity>().AddRange(entities);
         }
 
         public void Remove(TEntity entity)
         {
-            throw new NotImplementedException();
+            Context.Set<TEntity>().Remove(entity);
         }
 
         public void RemoveRange(IEnumerable<TEntity> entities)
         {
-            throw new NotImplementedException();
+            Context.Set<TEntity>().RemoveRange(entities);
         }
     }
 }
