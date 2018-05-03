d6e16c9d85d030aa5748adb6b49322f1776e94b7
newlandp <paulnewland@hotmail.com>
2018-05-03 09:42:39 -0400

    restructure default file configurations

--- RestaurantReviews/Core/Domain/Restaurant.cs
+++ RestaurantReviews/Core/Domain/Restaurant.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace RestaurantReviews.Models
+namespace RestaurantReviews.Core.Domain
 {
     public class Restaurant
     {
