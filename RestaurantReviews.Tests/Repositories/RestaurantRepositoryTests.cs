99a902b17686170bd4ac212ac870c8b7b4fdbbbb
newlandp <paulnewland@hotmail.com>
2018-05-07 19:28:26 -0400

    tests done and connection string updated

--- RestaurantReviews.Tests/Repositories/RestaurantRepositoryTests.cs
+++ RestaurantReviews.Tests/Repositories/RestaurantRepositoryTests.cs
@@ -1,39 +1,71 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestaurantReviews.Persistence;
+using RestaurantReviews.Persistence.Repositories;
 
 namespace RestaurantReviews.Tests.Repositories
 {
     [TestClass]
     public class RestaurantRepositoryTests
     {
+        private readonly RestaurantRepository _restaurantRepo = new RestaurantRepository(new ApplicationDbContext());
+
         [TestMethod]
         public void GetTopRatedRestaurantsTest()
         {
+            var restaurants = _restaurantRepo.GetTopRatedRestaurants(3);
+
+            Assert.AreEqual(restaurants.Count(), 3);
+            Assert.AreEqual(_restaurantRepo.GetAverageRating(restaurants.First()) >
+                            _restaurantRepo.GetAverageRating(restaurants.Last()), true);
+            Assert.AreNotEqual(restaurants.Count(), 4);
+            Assert.AreNotEqual(restaurants.Count(), 2);
         }
 
         [TestMethod]
         public void SearchRestaurantsByNameTest()
         {
+            var restaurants = _restaurantRepo.SearchRestaurantsByName("chi");
+
+            Assert.AreEqual(restaurants.Count(), 2);
+            Assert.AreEqual(restaurants.Any(r => r.Name == "Chipotle"), true);
+            Assert.AreNotEqual(restaurants.Count(), 3);
+            Assert.AreNotEqual(restaurants.Count(), 1);
+            Assert.AreNotEqual(restaurants.Any(r => r.Name == "McDonalds"), true);
         }
 
         [TestMethod]
         public void GetRestaurantsSortedByNameAZTest()
         {
+            var restaurants = _restaurantRepo.GetRestaurantsSortedByNameAZ();
+
+            Assert.AreEqual(restaurants.Count(), 10);
         }
 
         [TestMethod]
         public void GetRestaurantsSortedByNameZATest()
         {
+            var restaurants = _restaurantRepo.GetRestaurantsSortedByNameZA();
+
+            Assert.AreEqual(restaurants.Count(), 10);
         }
 
         [TestMethod]
         public void GetAverageRatingTest()
         {
+            var restaurant = _restaurantRepo.Get(1);
+            var avgRating = _restaurantRepo.GetAverageRating(restaurant);
+
+            Assert.AreEqual(avgRating < 5.0 && avgRating > 4.0, true);
         }
 
         [TestMethod]
         public void GetWithReviewsTest()
         {
+            var restaurant = _restaurantRepo.GetWithReviews(1);
+
+            Assert.AreEqual(restaurant.Reviews.First().Rating, 2);
         }
 }
 }
--- RestaurantReviews.Tests/Repositories/ReviewRepositoryTests.cs
+++ RestaurantReviews.Tests/Repositories/ReviewRepositoryTests.cs
@@ -1,19 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestaurantReviews.Core.Domain;
+using RestaurantReviews.Persistence;
+using RestaurantReviews.Persistence.Repositories;
 
 namespace RestaurantReviews.Tests.Repositories
 {
     [TestClass]
     public class ReviewRepositoryTests
     {
+        private readonly ReviewRepository _reviewRepo = new ReviewRepository(new ApplicationDbContext());
+
         [TestMethod]
         public void GetReviewsByUserTest()
         {
+            var reviews = _reviewRepo.GetReviewsByUser("817deb78-45fc-4b63-8eb0-b7fde8d627f4");
+
+            Assert.AreEqual(reviews.Any(r => r.Id == 4), true);
+            Assert.AreEqual(reviews.Any(r => r.Id == 5), true);
+            Assert.AreEqual(reviews.Any(r => r.Id == 6), true);
+            Assert.AreEqual(reviews.Any(r => r.Id == 17), true);
+            Assert.AreEqual(reviews.Any(r => r.Id == 30), true);
+            Assert.AreNotEqual(reviews.Any(r => r.Id == 1), true);
+            Assert.AreNotEqual(reviews.Any(r => r.Id == 2), true);
+            Assert.AreNotEqual(reviews.Any(r => r.Id == 3), true);
+            Assert.AreNotEqual(reviews.Any(r => r.Id == 25), true);
+            Assert.AreNotEqual(reviews.Any(r => r.Id == 20), true);
         }
 
         [TestMethod]
         public void GetReviewsByRestaurantTest()
         {
+            var reviews = _reviewRepo.GetReviewsByRestaurant(1);
+
+            Assert.AreEqual(reviews.Any(r => r.Id == 1), true);
+            Assert.AreEqual(reviews.Any(r => r.Id == 2), true);
+            Assert.AreEqual(reviews.Any(r => r.Id == 1), true);
+            Assert.AreNotEqual(reviews.Any(r => r.Id == 6), true);
+            Assert.AreNotEqual(reviews.Any(r => r.Id == 8), true);
+            Assert.AreNotEqual(reviews.Any(r => r.Id == 123), true);
         }
     }
 }
